e258c53ea3f8594a04b7b599400e97dc6fcf6e21
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-12 13:31:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-    if phrase == "I LOVE YOU GRANDMA!"
-      return 
+      return "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
   else 
-    "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
 end 
 
 
