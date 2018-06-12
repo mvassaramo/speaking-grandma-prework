15c06dfdaac0a9c1b1cc1defbf4a1ca8aee0e834
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-12 13:37:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,12 @@
 def speak_to_grandma
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif speak_to_grandma == speak_to_grandma.upcase
-      return "NO, NOT SINCE 1938!"
-  else 
+    return "NO, NOT SINCE 1938!"
+  else
     return "HUH?! SPEAK UP, SONNY!"
-end 
+  end
+end
 
 
 # Whatever you say to grandma, she should respond with
