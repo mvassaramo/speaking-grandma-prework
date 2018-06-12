5bcfce6998cf4ae069332e31398f5a9aed7c0396
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-12 13:27:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
-def speak_to_grandma(greeting)
-  if greeting = "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 end 
 
